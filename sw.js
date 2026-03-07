53456c8017803a6c291eee30859e638383f699e6
Scott Porter <265711253+Sftechlawyer@users.noreply.github.com>
2026-03-06 21:01:04 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "oa-log-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(["./"]);
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
